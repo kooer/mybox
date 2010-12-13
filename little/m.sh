3924b7d4da242c40fb060797ef7980e77bb89e8d
shafei <koooer@gmail.com>
2010-12-13 22:11:24 +0800

    better..

--- little/m.sh
+++ little/m.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 SAVEIFS=$IFS
 IFS=$(echo -en "\n\b")
-dir='/home/kooer/book/newbook/'
+dir='/home/kooer/book/newbook/b/'
 files=`ls $dir`
 for v in $files
 do
-    echo $v
-    echo "" | mutt -s "$v" -a "$dir$v" -- shafei1900@gmail.com
+    _subject=`ls -lh $dir$v | grep "^-" | awk '{print " [size:"$5"]"}'`
+    echo "$v $_subject"
+    echo "" | mutt -s "$v $_subject" -a "$dir$v" -- shafei1900@gmail.com
 done
 IFS=$SAVEIFS
